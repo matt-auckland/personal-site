5de42bb01f04a27e019425af71f770de5f22ffe9
Matt <mpau548@aucklanduni.ac.nz>
2020-08-24 20:22:51 +1200

    Add 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,22 @@
+const fs = require("fs");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync("_site/404.html");
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      },
+    },
+  });
+
   // Output directory: _site
 
   eleventyConfig.addFilter("tagUrl", function (tag) {
